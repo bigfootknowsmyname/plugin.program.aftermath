8f985cb7429bfbfc3fd3d668780faeea519e06d9
Unknown <drinfernoo@yahoo.com>
2018-08-07 09:06:42 -0700

    Base off of Surfacingx/Aftermath
    plugin.program.aftermath, v0.2.4

---
+++ uservar.py
@@ -0,0 +1,121 @@
+import os, xbmc, xbmcaddon
+
+#########################################################
+### User Edit Variables #################################
+#########################################################
+ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
+ADDONTITLE     = '[B][COLOR dodgerblue]Aftermath[/COLOR] [COLOR white]Wizard[/COLOR][/B]'
+EXCLUDES       = [ADDON_ID, 'repository.aftermath']
+# Enable/Disable the text file caching with 'Yes' or 'No' and age being how often it rechecks in minutes
+CACHETEXT      = 'Yes'
+CACHEAGE       = 30
+# Text File with build info in it.
+BUILDFILE      = 'http://cb.srfx.in/builds.txt'
+# How often you would list it to check for build updates in days
+# 0 being every startup of kodi
+UPDATECHECK    = 0
+# Text File with apk info in it.  Leave as 'http://' to ignore
+APKFILE        = 'http://cb.srfx.in/apks.txt'
+# Text File with Youtube Videos urls.  Leave as 'http://' to ignore
+YOUTUBETITLE   = ''
+YOUTUBEFILE    = 'http://'
+# Text File for addon installer.  Leave as 'http://' to ignore
+ADDONFILE      = 'http://cb.srfx.in/addons.txt'
+# Text File for advanced settings.  Leave as 'http://' to ignore
+ADVANCEDFILE   = 'http://aftermathwizard.net/testtext/advanced.txt'
+
+# Dont need to edit just here for icons stored locally
+PATH           = xbmcaddon.Addon().getAddonInfo('path')
+ART            = os.path.join(PATH, 'resources', 'art')
+
+#########################################################
+### THEMING MENU ITEMS ##################################
+#########################################################
+# If you want to use locally stored icons the place them in the Resources/Art/
+# folder of the wizard then use os.path.join(ART, 'imagename.png')
+# do not place quotes around os.path.join
+# Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
+#           ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
+# Leave as http:// for default icon
+ICONBUILDS     = os.path.join(ART, 'builds.png')
+ICONMAINT      = os.path.join(ART, 'Maintenance.png')
+ICONAPK        = os.path.join(ART, 'apkinstaller.png')
+ICONADDONS     = os.path.join(ART, 'addoninstaller.png')
+ICONYOUTUBE    = 'http://'
+ICONSAVE       = os.path.join(ART, 'savedata.png')
+ICONTRAKT      = os.path.join(ART, 'keeptrakt.png')
+ICONREAL       = os.path.join(ART, 'keepdebrid.png')
+ICONLOGIN      = os.path.join(ART, 'keeplogin.png')
+ICONCONTACT    = os.path.join(ART, 'information.png')
+ICONSETTINGS   = os.path.join(ART, 'settings.png')
+# Hide the ====== seperators 'Yes' or 'No'
+HIDESPACERS    = 'No'
+# Character used in seperator
+SPACER         = '='
+
+# You can edit these however you want, just make sure that you have a %s in each of the
+# THEME's so it grabs the text from the menu item
+COLOR1         = 'dodgerblue'
+COLOR2         = 'white'
+# Primary menu items   / %s is the menu item and is required
+THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']Aftermath[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
+# Build Names          / %s is the menu item and is required
+THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
+# Alternate items      / %s is the menu item and is required
+THEME3         = '[COLOR '+COLOR1+']%s[/COLOR]'
+# Current Build Header / %s is the menu item and is required
+THEME4         = '[COLOR '+COLOR1+']Current Build:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+# Current Theme Header / %s is the menu item and is required
+THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+
+# Message for Contact Page
+# Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
+HIDECONTACT    = 'No'
+# You can add \n to do line breaks
+CONTACT        = 'Thank you for choosing Aftermath Wizard.\n\nContact us on facebook at http://facebook.com'
+#Images used for the contact window.  http:// for default icon and fanart
+CONTACTICON    = os.path.join(ART, 'qricon.png')
+CONTACTFANART  = 'http://'
+#########################################################
+
+#########################################################
+### AUTO UPDATE #########################################
+########## FOR THOSE WITH NO REPO #######################
+# Enable Auto Update 'Yes' or 'No'
+AUTOUPDATE     = 'No'
+# Url to wizard version
+WIZARDFILE     = ''
+#########################################################
+
+#########################################################
+### AUTO INSTALL ########################################
+########## REPO IF NOT INSTALLED ########################
+# Enable Auto Install 'Yes' or 'No'
+AUTOINSTALL    = 'Yes'
+# Addon ID for the repository
+REPOID         = 'repository.aftermath'
+# Url to Addons.xml file in your repo folder(this is so we can get the latest version)
+REPOADDONXML   = 'https://raw.githubusercontent.com/surfacingx/Aftermath/master/repository.aftermath/addon.xml'
+# Url to folder zip is located in
+REPOZIPURL     = 'https://raw.githubusercontent.com/surfacingx/Aftermath/master/repository.aftermath/'
+#########################################################
+
+#########################################################
+### NOTIFICATION WINDOW##################################
+#########################################################
+# Enable Notification screen Yes or No
+ENABLE         = 'Yes'
+# Url to notification file
+NOTIFICATION   = 'http://cb.srfx.in/notify.txt'
+# Use either 'Text' or 'Image'
+HEADERTYPE     = 'Text'
+# Font size of header
+FONTHEADER     = 'Font14'
+HEADERMESSAGE  = '[B][COLOR dodgerblue]Aftermath[/COLOR] [COLOR white]Wizard[/COLOR][/B]'
+# url to image if using Image 424x180
+HEADERIMAGE    = ''
+# Font for Notification Window
+FONTSETTINGS   = 'Font13'
+# Background for Notification Window
+BACKGROUND     = 'http://cb.srfx.in/img/fanart.jpg'
+#########################################################
